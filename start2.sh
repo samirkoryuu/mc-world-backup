936941f6fd360dc188a72a03008f7ed817807efb
Samir Koryuu <bokuwasamitaro@gmail.com>
2026-09-02 10:29:21 +0600

    Create start2.sh

---
+++ start2.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+proot-distro login ubuntu
+
+playitd --secret "dac7bd3a003c58358bc746a0fc0b950fb11e35de3944a43f07c030c96029806a" --socket-path "$HOME/playit.sock"
