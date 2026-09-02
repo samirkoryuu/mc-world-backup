f616fb8c5ebbec0d3ee455d94f8d8fa2fe1ffef1
Samir Koryuu <bokuwasamitaro@gmail.com>
2026-09-02 10:16:11 +0600

    Create setup2.sh

---
+++ setup2.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+apt update
+
+apt install -y curl gnupg
+
+curl -SL https://playit-cloud.github.io/ppa/key.gpg -o /tmp/playit.gpg
+
+gpg --dearmor -o /etc/apt/trusted.gpg.d/playit.gpg /tmp/playit.gpg
+
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/playit.gpg] https://playit-cloud.github.io/ppa/data/ /" > /etc/apt/sources.list.d/playit-cloud.list
+
+apt update
+
+apt install -y playit
+
+exit
